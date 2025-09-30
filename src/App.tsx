dc9bb04823a683d08ccca95a67df21464dec4c51
Felipe <felipe.carvalho.dm@gmail.com>
2025-09-30 17:07:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+import './global.css'
+export function App() {
+
+
+  return (
+  <>
+    <h1>Bem vindo ao Pizza Shop</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  </>
+  )
+}
+
+

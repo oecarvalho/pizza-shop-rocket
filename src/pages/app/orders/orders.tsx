977972c1c5b01ee91c25cc1cd4c8afad9c4932ad
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-10-18 01:59:26 -0300

    finalizando filtros da tabela

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
 
 export function Orders(){
     return(
@@ -14,11 +14,8 @@ export function Orders(){
             </div>
 
             <div className="space-y-2.5 ">
-                <form action="" className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="nome do cliente" className="h-8 w-[320px]"/>
-                </form>
-
+                <OrderTableFilters/>
+                
                 <div className="border rounded-md">
                     <Table>
                         <TableHeader>
@@ -37,36 +34,7 @@ export function Orders(){
                         <TableBody>
                             {Array.from({length: 10}).map((_, i)=>{
                                 return (
-                            <TableRow key={i}>
-                                <TableCell>
-                                    <Button variant='outline' size='xs'>
-                                        <Search className="h-3 w-3"/>
-                                        <span className="sr-only">Detalhes do Pedido</span>
-                                    </Button>
-                                </TableCell>
-                                <TableCell className="font-mono text-xs font-medium">asd7a8s7da87szd87</TableCell>
-                                <TableCell className="text-muted-foreground">há 10 minutos</TableCell>
-                                <TableCell>
-                                    <div className="flex items-center gap-2">
-                                        <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                                        <span className="font-medium text-muted-foreground">Pendente</span>
-                                    </div>
-                                </TableCell>
-                                <TableCell className="font-medium">Felipe Carvalho</TableCell>
-                                <TableCell className="font-medium">R$ 149,90</TableCell>
-                                <TableCell>
-                                    <Button variant='outline' size='xs'>
-                                        <ArrowRight className="mr-2 h-3 w-3"/>
-                                        Aprovar
-                                    </Button>
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant='ghost' size='xs'>
-                                        <X className="mr-2 h-3 w-3"/>
-                                        Cancelar
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
+                                    <OrderTableRow key={i}/>
                                 )
                             })}
                         </TableBody>
